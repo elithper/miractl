ff13caef4a955bc53091b4d5aeba0a7da8400b65
elithper <contact@michaelhoward.kiwi>
2022-05-29 22:19:21 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+This repo contains a series of simple Python scripts for controlling Boox Mira e-Ink monitors.
---
+++ mira-refresh.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import usb.core
+import usb.util
+
+vid = 0x0416
+pid = 0x5020
+reattach = False
+
+# Create list of all Boox Mira devices
+device_list = usb.core.find(idVendor=vid, idProduct=pid, find_all=True)
+
+# Iterate through device list and refresh sequentially
+for dev in device_list:
+    dev.reset()
+    
+    if dev.is_kernel_driver_active(0):
+        reattach = True
+        dev.detach_kernel_driver(0)
+    
+    dev.set_configuration()
+    
+    dev.write(1, '\x01', 0)
+    
+    if reattach:
+        dev.attach_kernel_driver(0)
